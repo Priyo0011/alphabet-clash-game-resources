ae62ac2e6e19b7afd5380da2e885c01e6bb191a0
Priyo0011 <priyotosh0011@gmail.com>
2024-02-15 14:52:05 +0600

    game end section and esc btn add

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,17 +1,28 @@
 function handleKeyboardKeyUpEvent(event){
     const playerPressed = event.key;
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
 
     if(playerPressed === expectedAlphabet){
         console.log('you get a point');
-        console.log('you have pressed correctly', expectedAlphabet);
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else{
         console.log('you missed. you lost a life');
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+        if(updatedLife === 0){
+            gameOver();
+        }
     }
 }
 document.addEventListener('keyup', handleKeyboardKeyUpEvent);
@@ -28,17 +39,18 @@ function continueGame(){
 
 function play(){
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
     continueGame();
 }
 
-
-
-
-// function play(){
-//     const homeSection = document.getElementById('home-screen');
-//     homeSection.classList.add('hidden');
-
-//     const playgroundSection = document.getElementById('play-ground');
-//     playgroundSection.classList.remove('hidden');
-// }
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score', lastScore);
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
+}
